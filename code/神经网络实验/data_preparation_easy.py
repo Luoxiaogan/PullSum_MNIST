88efabcd73bfff4c9db888029c5cf527becf12a9
王若楠 <14846945+wrn0117@user.noreply.gitee.com>
2024-09-04 10:36:20 +0000

    24/9/4/v2

--- code/神经网络实验/data_preparation_easy.py
+++ code/神经网络实验/data_preparation_easy.py
@@ -64,4 +64,70 @@ def prepare_node_10_easy():
     
     return X_train_list, y_train_list, X_test, y_test
 
+# 将数据打乱并分给5个节点
+def prepare_node_5_shuffled():
+    X, y = mnist_data()
+    
+    # 将X和y转换为指定类型的NumPy数组
+    X = np.array(X, dtype=np.float32)
+    y = np.array(y, dtype=np.int64)
 
+    # 使用NumPy数组直接传递给train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+    # 转换回PyTorch张量
+    X_train = torch.tensor(X_train, dtype=torch.float32)
+    X_test = torch.tensor(X_test, dtype=torch.float32)
+    y_train = torch.tensor(y_train, dtype=torch.long)
+    y_test = torch.tensor(y_test, dtype=torch.long)
+
+    # 打乱训练数据
+    perm = torch.randperm(X_train.size(0))
+    X_train = X_train[perm]
+    y_train = y_train[perm]
+
+    # 将数据分为5份
+    chunk_size = len(X_train) // 5
+    h_data = [X_train[i * chunk_size:(i + 1) * chunk_size] for i in range(5)]
+    y_data = [y_train[i * chunk_size:(i + 1) * chunk_size] for i in range(5)]
+    
+    # 如果有剩余数据，将其分配给最后一个节点
+    if len(X_train) % 5 != 0:
+        h_data[-1] = torch.cat((h_data[-1], X_train[5 * chunk_size:]), dim=0)
+        y_data[-1] = torch.cat((y_data[-1], y_train[5 * chunk_size:]), dim=0)
+    
+    return h_data, y_data, X_test, y_test
+
+# 将数据打乱并分给10个节点
+def prepare_node_10_shuffled():
+    X, y = mnist_data()
+    
+    # 将X和y转换为指定类型的NumPy数组
+    X = np.array(X, dtype=np.float32)
+    y = np.array(y, dtype=np.int64)
+
+    # 使用NumPy数组直接传递给train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+    # 转换回PyTorch张量
+    X_train = torch.tensor(X_train, dtype=torch.float32)
+    X_test = torch.tensor(X_test, dtype=torch.float32)
+    y_train = torch.tensor(y_train, dtype=torch.long)
+    y_test = torch.tensor(y_test, dtype=torch.long)
+
+    # 打乱训练数据
+    perm = torch.randperm(X_train.size(0))
+    X_train = X_train[perm]
+    y_train = y_train[perm]
+
+    # 将数据分为10份
+    chunk_size = len(X_train) // 10
+    X_train_list = [X_train[i * chunk_size:(i + 1) * chunk_size] for i in range(10)]
+    y_train_list = [y_train[i * chunk_size:(i + 1) * chunk_size] for i in range(10)]
+    
+    # 如果有剩余数据，将其分配给最后一个节点
+    if len(X_train) % 10 != 0:
+        X_train_list[-1] = torch.cat((X_train_list[-1], X_train[10 * chunk_size:]), dim=0)
+        y_train_list[-1] = torch.cat((y_train_list[-1], y_train[10 * chunk_size:]), dim=0)
+    
+    return X_train_list, y_train_list, X_test, y_test
